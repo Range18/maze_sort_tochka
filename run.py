b3612d3ac41a2964997977bfc4fdfe8f5d15d6be
Range18 <shakmate@mail.ru>
2025-11-03 22:09:48 +0500

    All to one file run.py

--- run.py
+++ run.py
@@ -2,12 +2,127 @@ import sys
 import heapq
 from typing import Tuple, List
 
-from constants import COSTS, TARGETS
-from maze_state import MazeState
-from object_location import ObjectInfo
-from objects_service import is_all_in_right_room, is_obj_blocked_in_room, is_target_with_foreigners, get_type_objects, \
-    check_blocking_obj_in_hall, is_any_in_target, go_to_target
-from utils import get_depth, get_initial_state
+TARGETS = {"A": 2, "B": 4, "C": 6, "D": 8}
+COSTS = {"A": 1, "B": 10, "C": 100, "D": 1000}
+
+
+class ObjectInfo:
+    def __init__(self, obj_type: str, room_id: int = -1, depth: int = -1, hall_location: int = -1) -> None:
+        if room_id == -1 and hall_location == -1:
+            raise ValueError("Either room_id and depth or hall_location must be specified")
+        self.type = obj_type
+        self.room_id = room_id
+        self.depth = depth
+        self.hall_location = hall_location
+        self.energy = COSTS[self.type]
+
+    def __str__(self):
+        return f"{self.type}: room_id: {self.room_id} depth: {self.depth} hall_location: {self.hall_location}"
+
+    def __eq__(self, other):
+        return (self.type, self.room_id, self.depth, self.hall_location) == \
+            (other.type, other.room_id, other.depth, other.hall_location)
+
+    def __hash__(self):
+        return hash((self.type, self.room_id, self.depth, self.hall_location))
+
+
+class MazeState:
+    def __init__(self, locations: list[ObjectInfo], cost: int = 0) -> None:
+        self.locations = locations
+        self.cost = cost
+
+    def state_key(self) -> tuple:
+        return tuple(sorted((obj.type, obj.room_id, obj.depth, obj.hall_location) for obj in self.locations))
+
+    def is_finish_state(self) -> bool:
+        for obj in self.locations:
+            if obj.room_id != TARGETS[obj.type]:
+                return False
+        return True
+
+
+def get_initial_state(raw_rooms: list[str], depth: int) -> MazeState:
+    rows = [raw_room[3:10:2] for raw_room in raw_rooms]
+    print(rows)
+    locations = []
+    for i in range(depth):
+        for j in range(len(rows[i])):
+            locations.append(ObjectInfo(rows[i][j], (j + 1) * 2, i + 1, -1))
+    return MazeState(locations)
+
+
+def get_depth(maze: list[str]) -> int:
+    return len(maze) - 3
+
+
+def check_blocking_obj_in_hall(maze_state: list[ObjectInfo], obj: ObjectInfo, dest_room: int) -> bool:
+    loc = max(obj.hall_location, obj.room_id)
+
+    for other in maze_state:
+        if other == obj or other.hall_location == -1:
+            continue
+
+        if (loc <= other.hall_location <= dest_room) or (
+                dest_room <= other.hall_location <= loc):
+            return True
+    return False
+
+
+def get_type_objects(obj: ObjectInfo, state: MazeState) -> list[ObjectInfo]:
+    locations = []
+    for other in state.locations:
+        if other.type == obj.type and other is not obj:
+            locations.append(other)
+    return locations
+
+
+def is_all_in_right_room(obj: ObjectInfo, state: MazeState) -> bool:
+    if obj.room_id != TARGETS[obj.type]:
+        return False
+    depths = [obj.depth]
+    for other in get_type_objects(obj, state):
+        if other.room_id != TARGETS[obj.type]:
+            return False
+        depths.append(other.depth)
+    return depths == list(set(depths))
+
+
+def is_target_with_foreigners(obj: ObjectInfo, state: MazeState) -> bool:
+    target = TARGETS[obj.type]
+    for other in state.locations:
+        if other is obj or other.room_id == -1 or other.type == obj.type:
+            continue
+        if other.room_id == target:
+            return True
+    return False
+
+
+def is_any_in_target(obj: ObjectInfo, state: MazeState) -> bool:
+    target = TARGETS[obj.type]
+    for other in state.locations:
+        if other is obj or other.room_id == -1:
+            continue
+        if other.room_id == target:
+            return True
+    return False
+
+
+def is_obj_blocked_in_room(obj: ObjectInfo, state: MazeState) -> bool:
+    for other in state.locations:
+        if other is obj:
+            continue
+        if other.room_id == obj.room_id and other.depth != -1 and other.depth < obj.depth:
+            return True
+    return False
+
+
+def go_to_target(obj: ObjectInfo, obj_index: int, state: MazeState, depth: int, room_out_cost: int,
+                 object_location: int) -> MazeState:
+    cost_to_move = COSTS[obj.type] * (abs(object_location - TARGETS[obj.type]) + depth + room_out_cost)
+    locations_copy = state.locations.copy()
+    locations_copy[obj_index] = ObjectInfo(obj.type, room_id=TARGETS[obj.type], depth=depth)
+    return MazeState(locations_copy, state.cost + cost_to_move)
 
 
 def get_all_sub_states(state: MazeState, max_depth: int) -> list[MazeState]:
